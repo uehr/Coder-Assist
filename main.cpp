bf753ca7d21a0df34109bbcbd8a6d6b3cc21ce1c
yudai-uehara <uehara.yudai@gmail.com>
2017-04-24 12:10:01 +0900

    重複起動確認

--- main.cpp
+++ main.cpp
@@ -1,48 +1,103 @@
 #include <windows.h>
 #include <iostream>
+#include <psapi.h>
+#include <stdio.h>
+#include <string.h>
 
-#define toggle_key1 VK_LCONTROL
-#define toggle_key2 VK_LMENU
+#pragma comment( lib, "psapi.lib" )
+
+#define toggle_key1 VK_LCONTROL // ctrl
+#define toggle_key2 VK_LMENU // alt
 #define toggle_key3 'A'
-#define wait_nsec 300
+#define wait_nsec 500
+#define OPEN_PROCESS  (PROCESS_QUERY_INFORMATION | PROCESS_VM_READ)
 
 using namespace std;
 
-bool checked_key, aim_key_pushing, is_run = true;
+//二重起動防止
+int check_process_overlap() {
+    DWORD ProcessID[ 1024 ];
+    DWORD dwSize;
+    DWORD dwMax;
+    DWORD dwNow;
+    HANDLE hProcess;
+    int result_buf = 0;
 
-int add_aim_key(int check_key,int next_check_key,int put_key,bool shift_pushing){
+    // プロセス識別子の取得
+    EnumProcesses( ProcessID, sizeof(ProcessID), &dwSize );
+    dwMax = (dwSize / sizeof(DWORD));
+    // プロセス識別子からプロセス名を列挙
+    for ( dwNow = 0 ; dwNow < dwMax ; dwNow++ ){
+        TCHAR   szFile[ 1024 ] = { 0 };
+        HMODULE Module[ 1024 ] = { 0 };
+
+        // プロセスのフルパス名を取得
+        if ( (hProcess = OpenProcess(OPEN_PROCESS,FALSE,ProcessID[dwNow])) != NULL ){
+            if ( EnumProcessModules(hProcess,Module,sizeof(Module),&dwSize) ){
+                GetModuleFileNameEx( hProcess, Module[0], szFile, sizeof(szFile) );
+            }
+            CloseHandle( hProcess );
+        }
+        if(strstr(szFile,"Coder-Assist.exe") != NULL)
+          result_buf++;
+    }
+
+    //２つ以上プロセスが起動してたら1を返す
+    if(result_buf > 1)
+      return 1;
+
+    return 0;
+}
+
+int add_aim_key(int check_key,int next_check_key,int enter_key){
+  bool checked_key,
+       checked_next_key,
+       aim_key_pushing;
+
   aim_key_pushing= false;
-  checked_key = GetAsyncKeyState(check_key) && 0x80;
-
+  
   if(!next_check_key){
-    if(checked_key)
+    if(GetAsyncKeyState(check_key) && 0x80)
       aim_key_pushing = true;
   }else{
-    if(checked_key && (GetAsyncKeyState(next_check_key) && 0x80))
+    if((GetAsyncKeyState(check_key) && 0x80) && (GetAsyncKeyState(next_check_key) && 0x80))
       aim_key_pushing = true;
   }
 
   if(aim_key_pushing){
-    if(shift_pushing){
+    //補完記号が シフト + Nキー ならシフトを押す
+    if(VK_SHIFT == (check_key | next_check_key))
       keybd_event(VK_SHIFT,0,0,0);
-    }
-    keybd_event(put_key,0,0,0);
-    keybd_event(put_key,0,KEYEVENTF_KEYUP,0);
-    if(shift_pushing){
+
+    keybd_event(enter_key,0,0,0);
+    keybd_event(enter_key,0,KEYEVENTF_KEYUP,0);
+
+    if(VK_SHIFT == (check_key | next_check_key))
       keybd_event(VK_SHIFT,0,KEYEVENTF_KEYUP,0);
-    }
+
+    //補完後、一文字分左へ移動
     keybd_event(VK_LEFT,0,0,0);
     keybd_event(VK_LEFT,0,KEYEVENTF_KEYUP,0);
     Sleep(wait_nsec);
   }
+
   return 0;
 }
 
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ){
+  bool is_run = true;
+
+  if(check_process_overlap() == 1) {
+    MessageBeep( MB_OK );
+    MessageBox(NULL, "is already started", "Coder-Assist", MB_OK);
+    return -1;
+  };
+
   MessageBox(NULL, "Coder-Assist started", "Coder-Assist", MB_OK);
 
   while(true){
+    //トグルキーが押されるとon/off切り替え
     if((GetAsyncKeyState(toggle_key1) && 0x80) &&
        (GetAsyncKeyState(toggle_key2) && 0x80) &&
        (GetAsyncKeyState(toggle_key3) && 0x80))
@@ -50,12 +105,13 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
       is_run = !is_run;
       Sleep(1000);
     }
+
     if(is_run){
-        add_aim_key(VK_OEM_4, 0, VK_OEM_6, false);
-        add_aim_key(VK_OEM_COMMA, VK_SHIFT, VK_OEM_PERIOD, true);
-        add_aim_key('8', VK_SHIFT,'9', true);
-        add_aim_key('2', VK_SHIFT,'2', true);
-        add_aim_key('7', VK_SHIFT,'7', true);
+        add_aim_key(VK_OEM_4, 0, VK_OEM_6); //[],「」, {}, 【】
+        add_aim_key(VK_OEM_COMMA, VK_SHIFT, VK_OEM_PERIOD); //<>
+        add_aim_key('8', VK_SHIFT, '9'); //()
+        add_aim_key('2', VK_SHIFT, '2'); //""
+        add_aim_key('7', VK_SHIFT, '7'); //''
     }
     Sleep(10);
   }
